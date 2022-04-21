87952b8037eb249e3596b599d8f033b64bb84f1f
Jacobtread <jacobtread@gmail.com>
2022-04-21 20:38:19 +1200

    Firmware source and started app UI

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# DuckC
+# DuckCentral
+
+DuckCentral is an Android app for communicating with the [WifiDuck (https://github.com/SpacehuhnTech/WiFiDuck)](https://github.com/SpacehuhnTech/WiFiDuck). This removes the need for a web app which can free up some space
+on the duck as well as removing the need to remember the IP address of the web interface.
+
+## Modified Firmware
+
+DuckCentral has a modified firmware which removes the web interface. You don't have to use this and it is optional.
+This app is backwards compatible with the standard firmware. The firmware provided is a modified version of
+[https://github.com/SpacehuhnTech/WiFiDuck](https://github.com/SpacehuhnTech/WiFiDuck)
+
+The firmware source is stored under ./firmware and if you have the arduino-cli in your PATH you can use the
+gradle task "buildFirmware" to run the build
+
+Make sure you add "https://raw.githubusercontent.com/SpacehuhnTech/arduino/main/package_spacehuhn_index.json" to the additional
+urls section in your arduino-cli config file othewrise you will be missing the required board. You will also need to run
+`arduino-cli core update-index` and `arduino-cli core install wifiduck:esp8266`
--- app/src/main/java/com/jacobtread/duck/MainActivity.kt
+++ app/src/main/java/com/jacobtread/duck/MainActivity.kt
@@ -3,25 +3,94 @@ package com.jacobtread.duck
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.jacobtread.duck.ui.theme.DuckCentralTheme
+
+const val HOST_ADDR = "192.168.4.1"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DuckCentralTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
     }
+}
+
+sealed class Screen(val route: String, val icon: ImageVector, val name: String) {
+    object Home : Screen("home", Icons.Filled.Home, "Home")
+    object Files : Screen("files", Icons.Filled.Home, "Files")
+    object Terminal : Screen("terminal", Icons.Filled.Home, "Terminal")
+    object Settings : Screen("settings", Icons.Filled.Home, "Settings")
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    val items = listOf(
+        Screen.Home,
+        Screen.Files,
+        Screen.Terminal,
+        Screen.Settings
+    )
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = { Icon(screen.icon, contentDescription = null) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) {
+        NavHost(navController, startDestination = "home") {
+            composable(Screen.Home.route) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Home(navController: NavHostController) {
+
 }
 
 @Composable
