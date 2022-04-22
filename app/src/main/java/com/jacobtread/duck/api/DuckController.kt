78b5c927daae8beb2d104b4ecc950b057006bc43
Jacobtread <jacobtread@gmail.com>
2022-04-22 23:51:12 +1200

    Rename and fixed issues

---
+++ app/src/main/java/com/jacobtread/duck/api/DuckController.kt
@@ -0,0 +1,124 @@
+package com.jacobtread.duck.api
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+fun interface ResponseConsumer<R> {
+    fun consume(value: R)
+}
+
+typealias MessageQueue = ArrayDeque<QueueItem<*>>
+
+data class QueueItem<R>(val message: Message<R>, val handler: ResponseConsumer<R>)
+
+object DuckController {
+
+    // The default web socket server host
+    private const val HOST_ADDR = "192.168.4.1"
+
+    // The default web socket server port
+    private const val HOST_PORT = 80
+
+    // Interval between requesting a status update (1 second)
+    private const val STATUS_UPDATE_INTERVAL: Long = 1000;
+
+    // Whether status updates should be requested automatically
+    var statusUpdates: Boolean = true;
+
+
+    // The queue of messages to be sent along with their handlers
+    private val queue = MessageQueue()
+
+    // The state representing the status of the socket
+    var status by remember { mutableStateOf("waiting") }
+
+    // A handler for updating the status based on the consumed response
+    private val statusHandler = ResponseConsumer<String> { status = it }
+
+    // The time in milliseconds of the last status update
+    private var lastStatusUpdate = -1L
+
+    /**
+     * push Pushes a new message to the end of the queue.
+     *
+     * @param R The response type of the message
+     * @param message The message to add to the send queue
+     * @param handler The handler for when the message is sent and the response is received
+     */
+    fun <R> push(message: Message<R>, handler: ResponseConsumer<R>) {
+        synchronized(queue) {
+            queue.addLast(QueueItem(message, handler))
+        }
+    }
+
+    /**
+     * pushStatus Shortcut method for sending status requests to the server
+     * and handling them with status handler which updates the internal state
+     */
+    fun pushStatus() = push(StatusMessage(), statusHandler)
+
+    /**
+     * connect Connects to the web socket server
+     *
+     * @param host The host of the web socket server
+     * @param port The port of the web socket server
+     */
+    suspend fun connect(host: String = HOST_ADDR, port: Int = HOST_PORT) {
+        val client = HttpClient(Android) {
+            install(WebSockets)
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            client.webSocket(method = HttpMethod.Get, host = host, port = port) {
+                run(this)
+
+            }
+        }
+    }
+
+    /**
+     * run Runs the connection loop for the websocket which processes
+     * the outbound messages along with the inbound responses
+     *
+     * @param session The websocket session
+     */
+    private suspend fun run(session: WebSocketSession) {
+        while (true) {
+            while (queue.isNotEmpty()) {
+                val item = queue.removeFirst();
+                consumeItem(item, session)
+            }
+            if (statusUpdates) {
+                val time = System.currentTimeMillis();
+                if (time - lastStatusUpdate >= STATUS_UPDATE_INTERVAL) {
+                    lastStatusUpdate = time;
+                    pushStatus()
+                }
+            }
+        }
+    }
+
+    /**
+     * consumeItem This function only exists to ensure that type
+     * checking can successfully link the return type of the
+     * message with the consumer input type. Otherwise, Kotlin will
+     * assume the type as Any? causing type checking to fail
+     *
+     * @param R The response type
+     * @param item The item to consume
+     * @param session The session to consume on
+     */
+    private suspend fun <R> consumeItem(item: QueueItem<R>, session: WebSocketSession) {
+        val response = session.message(item.message)
+        item.handler.consume(response)
+    }
+}
