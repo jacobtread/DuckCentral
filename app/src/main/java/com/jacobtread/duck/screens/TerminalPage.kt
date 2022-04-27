db5101d1e7895d1329a94710a13231befa39a24a
Jacobtread <jacobtread@gmail.com>
2022-04-27 17:59:35 +1200

    Rearranged terminal page and adjusted weight of input field

--- app/src/main/java/com/jacobtread/duck/screens/TerminalPage.kt
+++ app/src/main/java/com/jacobtread/duck/screens/TerminalPage.kt
@@ -29,46 +29,6 @@ import kotlinx.coroutines.runBlocking
 object TerminalPage : Page("terminal", Icons.Filled.Code, "Terminal") {
 
     @Composable
-    fun UserInput(modifier: Modifier) {
-
-        Row(modifier) {
-            val (message, setMessage) = remember { mutableStateOf("") }
-
-            TextField(
-                value = message,
-                onValueChange = setMessage,
-            )
-            IconButton(onClick = {
-                if (message.isNotBlank()) {
-                    DuckController.push(TerminalMessage(message)) {}
-                }
-            }) {
-                Icon(Icons.Filled.Send, contentDescription = "Send")
-            }
-
-        }
-    }
-
-    @Composable
-    fun Messages(
-        lines: List<String>,
-        scrollState: LazyListState,
-        modifier: Modifier = Modifier,
-    ) {
-        LazyColumn(
-            state = scrollState,
-            modifier = modifier
-        ) {
-            for (index in lines.indices) {
-                item {
-                    val line = lines[index]
-                    Text(line)
-                }
-            }
-        }
-    }
-
-    @Composable
     override fun Root(navController: NavHostController, modifier: Modifier) {
         val lines = remember { mutableStateListOf<String>() }
         val scrollState = rememberLazyListState()
@@ -108,4 +68,47 @@ object TerminalPage : Page("terminal", Icons.Filled.Code, "Terminal") {
             )
         }
     }
+
+    @Composable
+    fun Messages(
+        lines: List<String>,
+        scrollState: LazyListState,
+        modifier: Modifier = Modifier,
+    ) {
+        LazyColumn(
+            state = scrollState,
+            modifier = modifier
+        ) {
+            for (index in lines.indices) {
+                item {
+                    val line = lines[index]
+                    Text(line)
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun UserInput(modifier: Modifier) {
+
+        Row(modifier) {
+            val (message, setMessage) = remember { mutableStateOf("") }
+
+            TextField(
+                value = message,
+                onValueChange = setMessage,
+                modifier = Modifier.weight(1f)
+            )
+            IconButton(onClick = {
+                if (message.isNotBlank()) {
+                    DuckController.push(TerminalMessage(message)) {}
+                }
+            }) {
+                Icon(Icons.Filled.Send, contentDescription = "Send")
+            }
+
+        }
+    }
+
 }
