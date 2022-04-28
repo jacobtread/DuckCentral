12c140b95e699caac17c9b233bf55dade951b91a
Jacobtread <jacobtread@gmail.com>
2022-04-28 23:06:25 +1200

    Used retry flow on settings page

--- app/src/main/java/com/jacobtread/duck/pages/SettingsPage.kt
+++ app/src/main/java/com/jacobtread/duck/pages/SettingsPage.kt
@@ -6,46 +6,52 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
 import com.jacobtread.duck.components.Loader
 import com.jacobtread.duck.components.RetryLayout
+import com.jacobtread.duck.flow.RetryFlow
+import com.jacobtread.duck.socket.DuckController
 import com.jacobtread.duck.socket.command.commands.FileResponse
+import com.jacobtread.duck.socket.command.commands.FilesCommand
+import com.jacobtread.duck.socket.command.commands.SettingsCommand
+import com.jacobtread.duck.socket.command.commands.SettingsResponse
 
 object SettingsPage : Page("Settings", "settings", Icons.Filled.Settings) {
 
     class SettingsState {
-        var load by mutableStateOf(false)
-        var failed by mutableStateOf(false)
+        private var original: SettingsResponse? = null
 
         var ssid by mutableStateOf("")
         var channel by mutableStateOf("")
         var password by mutableStateOf("")
         var autorun by mutableStateOf("")
+
         val files = mutableListOf<FileResponse>()
+
+        fun load(settings: SettingsResponse, files: List<FileResponse>) {
+            original = settings
+            ssid = settings.ssid
+            password = settings.password
+            channel = settings.channel
+            autorun = settings.autorun
+            this.files.addAll(files)
+        }
     }
 
     @Composable
     override fun Content() {
-        Text("Settings Page")
-
         val state = remember { SettingsState() }
-
-        LaunchedEffect(state.load) {
-            if (state.load) {
-                try {
-
-                } catch (e: Throwable) {
-                    state.failed = true
-                }
-                state.load = false
-            }
+        RetryFlow(
+            load = {
+                val settings = DuckController.send(SettingsCommand())
+                val files = DuckController.send(FilesCommand())
+                state.load(settings, files)
+            },
+            errorTitle = "Failed to load",
+            loadingTitle = "Loading Settings",
+            loadingMessage = "Retrieving settings from websocket"
+        ) {
+            Text("Settings Page")
+            Text(state.ssid)
+            Text(state.password)
         }
-
-        if (state.load) {
-            Loader("Loading Settings", "Retrieving settings from websocket")
-        } else if(state.failed){
-            RetryLayout("Failed to load", "Unable to load settings") {
-
-            }
-        }
-
     }
 
 }
